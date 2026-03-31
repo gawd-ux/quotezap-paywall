2db41450570681f977eb181bb0f1411fc251d3fd
new user <newuser@Admins-MacBook-Air-2.local>
2026-03-31 10:07:08 +1000

    hardcode firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyA9pB1178DkEHHdXwRGoPdLUrZT0SnmcGA",
+  authDomain: "quotezap-7c5a3.firebaseapp.com",
+  databaseURL: "https://quotezap-7c5a3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "quotezap-7c5a3",
+  storageBucket: "quotezap-7c5a3.firebasestorage.app",
+  messagingSenderId: "803604344652",
+  appId: "1:803604344652:web:13b3dedc667c1b2231e9e9",
+  measurementId: "G-FR5LLFG1Y9"
 };
 
 const app = initializeApp(firebaseConfig);
