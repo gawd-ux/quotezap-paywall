b6ed3e723da9acc9c38add5d7a9fa3cb9b0e5753
new user <newuser@Admins-MacBook-Air-2.local>
2026-03-31 13:16:15 +1000

    fix asset paths to relative

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,6 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], viteSingleFile()],
-  base: "/quotezap.com.au/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  base: "./", // ✅ RELATIVE PATH (this fixes everything)
+  plugins: [react(), tailwindcss(), viteSingleFile()],
 });
