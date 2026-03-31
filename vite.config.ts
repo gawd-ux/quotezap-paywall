4e696c60c0d486b8609e795ff2a3c0dfbbaa72b4
new user <newuser@Admins-MacBook-Air-2.local>
2026-03-31 10:13:56 +1000

    fix asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/",
+  plugins: [react(), tailwindcss()], viteSingleFile()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
